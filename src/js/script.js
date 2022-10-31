dd7343163680f50a54019e63ac1d10258ab9245a
Andrey Voronov <my.web.vaa@gmail.com>
2022-10-31 17:57:32 +0200

    v.2.1. refresh window - done

--- src/js/script.js
+++ src/js/script.js
@@ -10,11 +10,11 @@ window.addEventListener('DOMContentLoaded', () => {
         addNewProductForm = [];
 
     body.addEventListener('click', (event) => {
+        event.preventDefault();
         switch (event.target.dataset.btn) {
             case 'refresh-stocks': 
                 openRefreshWindow();
                 addNewProductForm = document.querySelector('.products__form'); 
-                console.log(body.children[5].children[0].childNodes[1].childNodes);
                 break;
             case 'shoping-list':
                 //Вставит приветствие Hello "nickname"! What do we do?
@@ -24,24 +24,27 @@ window.addEventListener('DOMContentLoaded', () => {
                 refreshWindow.classList.add('hide');         
                 break;
             case 'add-product':
-                addForm.insertAdjacentElement('afterend', addNewProduct(
-                    addNewProductForm.childNodes[0].value,
-                    addNewProductForm.childNodes[2].value,
-                    addNewProductForm.childNodes[4].value,
-                    addNewProductForm.childNodes[6].value,
-                    addNewProductForm.childNodes[8].value
-                ));
-                //сделать отправку новых данных на сервер
-                addNewProductForm.childNodes[0].value = '';
-                addNewProductForm.childNodes[2].value = '';
-                addNewProductForm.childNodes[4].value = '';
-                addNewProductForm.childNodes[6].value = '';
-                addNewProductForm.childNodes[8].value = '';
+                if (!addNewProductForm.childNodes[0].value || addNewProductForm.childNodes[0].value === ' ') {
+                    addNewProductForm.childNodes[0].placeholder = 'Введите название продукта';
+                } else {
+                    addForm.insertAdjacentElement('afterend', addNewProduct(
+                        addNewProductForm.childNodes[0].value,
+                        addNewProductForm.childNodes[2].value,
+                        addNewProductForm.childNodes[4].value,
+                        addNewProductForm.childNodes[6].value,
+                        addNewProductForm.childNodes[8].value
+                    ));
+                    //сделать отправку новых данных на сервер
+                    addNewProductForm.childNodes[0].value = '';
+                    addNewProductForm.childNodes[2].value = '';
+                    addNewProductForm.childNodes[4].value = '';
+                    addNewProductForm.childNodes[6].value = '';
+                    addNewProductForm.childNodes[8].value = '';
+                }
                 break;
             case 'change-product':
                 productsItems = document.querySelectorAll('.products__item');
-                console.log(productsItems);
-                if (event.target.innerHTML === "Change") {
+                if (event.target.innerHTML === "Edit") {
                     event.target.innerHTML = "Cancel";
                     event.target.style.backgroundColor = 'rgb(244, 204, 43)';
                     productsItems.forEach(item => {
@@ -52,7 +55,7 @@ window.addEventListener('DOMContentLoaded', () => {
                         }
                     });    
                 } else {
-                    event.target.innerHTML = "Change";
+                    event.target.innerHTML = "Edit";
                     event.target.style.backgroundColor = 'rgb(81, 167, 242)';
                     productsItems.forEach(item => {
                         if (event.target.dataset.btnkey === item.dataset.obj) {
@@ -76,7 +79,7 @@ window.addEventListener('DOMContentLoaded', () => {
                         item.childNodes[7].childNodes[3].placeholder = item.childNodes[7].childNodes[3].value;
                         item.childNodes[7].childNodes[7].placeholder = item.childNodes[7].childNodes[7].value;
                         item.childNodes[7].childNodes[11].placeholder = item.childNodes[7].childNodes[11].value;
-                        item.childNodes[5].innerHTML = "Change";
+                        item.childNodes[5].innerHTML = "Edit";
                         item.childNodes[5].style.backgroundColor = 'rgb(81, 167, 242)';
                         item.childNodes[7].classList.toggle('hide'); //под 7 номером у родителя находится елемент с тегом .btn__wrap
                         item.childNodes[1].disabled = true;
@@ -106,29 +109,15 @@ window.addEventListener('DOMContentLoaded', () => {
         request.send();
         request.addEventListener('load', () => {
             if (request.status === 200) {
-                const data = JSON.parse(request.response);
-                const newItem = document.createElement('div');
-                newItem.classList.add('products__item__wrap');
+                const data = JSON.parse(request.response);    
                 for (let key in data) {
-                    newItem.innerHTML += `
-                    <div data-obj="${key}" data-name="${data[key].name}" class="products__item">
-                        <input required disabled placeholder="${data[key].name}" value="${data[key].name}" name="product-name" type="text" class="input input_new-product input_product">
-                        <input required disabled placeholder="${data[key].quantity}" value="${data[key].quantity}" name="product-quantity" type="text" class="input input_new-quantity input_quantity">
-                        <button data-btnkey="${key}" data-btn="change-product" class="btn btn_change-product">Change</button>
-                        <div data-wrap="${key}" class="btn__wrap hide">
-                            <div class="products__price">Price for 1 unit</div>
-                            <input required placeholder="${data[key].price}" value="${data[key].price}" name="product-price" type="text" class="input input_price">
-                            <div class="products__group">Product group</div>
-                            <input required placeholder="${data[key].group}" value="${data[key].group}" name="product-group" type="text" class="input input_group">
-                            <div class="products__stock">Stock</div>
-                            <input required placeholder="${data[key].stock}" value="${data[key].stock}" name="product-stock" type="text" class="input input_stock">
-                            <button data-btnkey="${key}" data-btn="save-product" class="btn btn_refresh-product">Save</button>
-                            <button data-btnkey="${key}" data-btn="del-product" class="btn btn_delete-product">Del</button>
-                        </div>
-                    </div>
-                `
-                };
-                addForm.insertAdjacentElement('afterend', newItem);
+                    addForm.insertAdjacentElement('afterend', addNewProduct(
+                        data[key].name, 
+                        data[key].quantity, 
+                        data[key].price, 
+                        data[key].group, 
+                        data[key].stock));    
+                }
             } else {
             //Вставить сообщение об ошибке
             }
@@ -136,23 +125,27 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function addNewProduct(name, quantity, price, group, stock) {
+        if (quantity === '') {quantity = 0};
+        if (price === '') {price = 0};
+        if (group === '') {group = 1};
+        if (stock === '') {stock = 0};
         const newProduct = document.createElement('div');
-        newProduct.classList.add('products__item__wrap');
+        newProduct.classList.add('products__item');
+        newProduct.dataset.obj = name;
+        newProduct.dataset.name = name;
         newProduct.innerHTML = `
-            <div data-obj="${name}" data-name="${name}" class="products__item">
-                <input required disabled placeholder="${name}" value="${name}" name="product-name" type="text" class="input input_new-product input_product">
-                <input required disabled placeholder="${quantity}" value="${quantity}" name="product-quantity" type="text" class="input input_new-quantity input_quantity">
-                <button data-btnkey="${name}" data-btn="change-product" class="btn btn_change-product">Change</button>
-                <div data-wrap="${name}" class="btn__wrap hide">
-                    <div class="products__price">Price for 1 unit</div>
-                    <input required placeholder="${price}" value="${price}" name="product-price" type="text" class="input input_price">
-                    <div class="products__group">Product group</div>
-                    <input required placeholder="${group}" value="${group}" name="product-group" type="text" class="input input_group">
-                    <div class="products__stock">Stock</div>
-                    <input required placeholder="${stock}" value="${stock}" name="product-stock" type="text" class="input input_stock">
-                    <button data-btnkey="${name}" data-btn="save-product" class="btn btn_refresh-product">Save</button>
-                    <button data-btnkey="${name}" data-btn="del-product" class="btn btn_delete-product">Del</button>
-                </div>
+            <input required disabled placeholder="${name}" value="${name}" name="product-name" type="text" class="input input_new-product input_product">
+            <input required disabled placeholder="${quantity}" value="${quantity}" name="product-quantity" type="text" class="input input_new-quantity input_quantity">
+            <button data-btnkey="${name}" data-btn="change-product" class="btn btn_change-product">Edit</button>
+            <div data-wrap="${name}" class="btn__wrap hide">
+                <div class="products__price">Price for 1 unit</div>
+                <input required placeholder="${price}" value="${price}" name="product-price" type="text" class="input input_price">
+                <div class="products__group">Product group</div>
+                <input required placeholder="${group}" value="${group}" name="product-group" type="text" class="input input_group">
+                <div class="products__stock">Stock</div>
+                <input required placeholder="${stock}" value="${stock}" name="product-stock" type="text" class="input input_stock">
+                <button data-btnkey="${name}" data-btn="save-product" class="btn btn_refresh-product">Save</button>
+                <button data-btnkey="${name}" data-btn="del-product" class="btn btn_delete-product">Del</button>
             </div>
         `;
         return newProduct;
